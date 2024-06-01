129812ef047ab981e4c4623a0d412f783ad768cf
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-06-01 22:53:39 +0530

    get repos done

--- controller/getrepos.controller.js
+++ controller/getrepos.controller.js
@@ -2,13 +2,14 @@ const { response } = require("express");
 
 const getrepo = async (req, res) => {
     const token = req.query.token;
+    const user = req.query.user;
     const options = {
         headers: {
             "Accept": "application/json",
             "Authorization": `Bearer ${token}`
         }
     }
-    await fetch('https://api.github.com/user/repos', options)
+    await fetch(`https://api.github.com/search/repositories?q=user:${user}`, options)
         .then(response => response.json())
         .then(data => res.send(data))
 }
